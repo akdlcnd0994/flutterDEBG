edb02b3d4a6346e1d24c9a3c39eb0193b650244c
양휘준 <akdlcnd0994@naver.com>
2023-10-05 18:25:07 +0900

    Update receiver_row_view.dart

--- lib/widget/chat/receiver_row_view.dart
+++ lib/widget/chat/receiver_row_view.dart
@@ -1,6 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:medicalapp/model/global_members.dart';
 import 'package:medicalapp/screens/qna_screen.dart';
+import 'package:http/http.dart' as http;
+
+const apiKey = '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';
+const apiUrl = 'https://api.openai.com/v1/completions';
 
 class ReceiverRowView extends StatelessWidget {
   const ReceiverRowView({Key? key, required this.index}) : super(key: key);
@@ -37,6 +43,62 @@ class ReceiverRowView extends StatelessWidget {
       subtitle: const Padding(
         padding: EdgeInsets.only(left: 8, top: 4),
         child: Text('8:04 AM', style: TextStyle(fontSize: 10)),
+      ),
+    );
+  }
+}
+
+Future<String> generateText(String prompt) async {
+  final response = await http.post(
+    Uri.parse(apiUrl),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $apiKey'
+    },
+    body: jsonEncode({
+      "model": "text-davinci-003",
+      'prompt': prompt,
+      'max_tokens': 1000,
+      'temperature': 0,
+      'top_p': 1,
+      'frequency_penalty': 0,
+      'presence_penalty': 0
+    }),
+  );
+
+  Map<String, dynamic> newresponse =
+      jsonDecode(utf8.decode(response.bodyBytes));
+
+  return newresponse['choices'][0]['text'];
+}
+
+class ResultPage extends StatefulWidget {
+  final String prompt;
+  const ResultPage(this.prompt, {super.key});
+
+  @override
+  State<ResultPage> createState() => _ResultPageState();
+}
+
+class _ResultPageState extends State<ResultPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Result from GPT"),
+      ),
+      // body: Text(widget.prompt),
+      body: FutureBuilder<String>(
+        future: generateText(widget.prompt),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            return Text('${snapshot.data}');
+          }
+        },
       ),
     );
   }
