835aa69a45b308dae04a3ac0cbfeb6cc12c3fe38
양휘준 <akdlcnd0994@naver.com>
2023-11-01 19:50:28 +0900

    search

--- lib/screens/health_info_list_screen.dart
+++ lib/screens/health_info_list_screen.dart
@@ -15,9 +15,10 @@ class HealthInfoListScreen extends StatefulWidget {
 
 class _HealthInfoListScreenState extends State<HealthInfoListScreen> {
   int page = 0;
-
+  Row rt = const Row();
   List<String> list = [];
-
+  int temp = 0;
+  final TextEditingController _tf = TextEditingController();
   late List<Widget> lw;
 
   @override
@@ -34,105 +35,183 @@ class _HealthInfoListScreenState extends State<HealthInfoListScreen> {
     return Scaffold(
         resizeToAvoidBottomInset: false,
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text(widget.part),
-        ),
-        body: ListView(
-          children: lw = pageList(context, height, width),
+        appBar: appBarMethod(context, height, width),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                children: lw = pageList(context, height, width),
+              ),
+            ),
+            rt,
+          ],
         ));
   }
 
-  List<Widget> pageList(BuildContext context, double height, double width) {
-    int temp = list.length;
-    print(temp);
+  AppBar appBarMethod(BuildContext context, double height, double width) {
+    if (widget.part == '') {
+      return AppBar(
+        iconTheme: const IconThemeData(
+          color: Colors.black, //색변경
+        ),
+        backgroundColor: Colors.white,
+        title: Container(
+          child: Row(
+            children: [
+              Flexible(
+                flex: 1,
+                child: TextField(
+                  controller: _tf,
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal: 16,
+                    ),
+                    border: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(8),
+                      ),
+                    ),
+                    labelText: widget.keyword,
+                    hintText: '검색 키워드를 입력해주세요',
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              TextButton(
+                onPressed: () {
+                  widget.keyword = _tf.text;
+                  page = 0;
+                  search();
+                  lw = pageList(context, height, width);
+                  setState(() {});
+                },
+                child: const Text(
+                  '검색',
+                  style: TextStyle(color: Colors.teal),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    } else {
+      return AppBar(
+        iconTheme: const IconThemeData(
+          color: Colors.black, //색변경
+        ),
+        backgroundColor: Colors.white,
+        title: Text(
+          widget.part,
+          style:
+              const TextStyle(color: Colors.teal, fontWeight: FontWeight.w600),
+        ),
+      );
+    }
+  }
+
+  Row RowMethod(BuildContext context, double height, double width) {
+    temp = list.length;
     temp = temp ~/ 15;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        if (page > 0)
+          TextButton(
+            child: const Text('<<'),
+            onPressed: () {
+              page -= 1;
+
+              setState(() {
+                lw = pageList(context, height, width);
+              });
+            },
+          )
+        else
+          TextButton(
+            child: const Text(''),
+            onPressed: () {},
+          ),
+        const SizedBox(width: 20),
+        Text((page + 1).toString()),
+        const SizedBox(width: 20),
+        if (page < temp)
+          TextButton(
+            child: const Text('>>'),
+            onPressed: () {
+              page += 1;
+
+              setState(() {
+                lw = pageList(context, height, width);
+              });
+            },
+          )
+        else
+          TextButton(
+            child: const Text(''),
+            onPressed: () {},
+          )
+      ],
+    );
+  }
+
+  List<Widget> pageList(BuildContext context, double height, double width) {
+    rt = RowMethod(context, height, width);
 
     return <Widget>[
       Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Column(
-            children: [
-              for (int i = page * 15;
-                  i < list.length && i < (page * 15) + 15;
-                  i++)
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(color: Colors.grey.withOpacity(0.4)),
-                    ),
-                  ),
-                  child: Material(
-                    color: Colors.white,
-                    child: InkWell(
-                      splashColor: Colors.grey,
-                      onTap: () {
-                        DictionaryInfo().sendDataToJSP(context, list[i]);
-                      },
-                      child: SizedBox(
-                        height: height * 0.0545,
-                        width: width,
-                        child: Padding(
-                          padding: EdgeInsets.only(left: (width * 0.1)),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Text(
-                                list[i],
+          for (int i = page * 15; i < list.length && i < (page * 15) + 15; i++)
+            Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(color: Colors.grey.withOpacity(0.4)),
+                ),
+              ),
+              child: Material(
+                color: Colors.white,
+                child: InkWell(
+                  splashColor: Colors.grey,
+                  onTap: () {
+                    DictionaryInfo().sendDataToJSP(context, list[i]);
+                  },
+                  child: SizedBox(
+                    height: height * 0.0525,
+                    width: width,
+                    child: Padding(
+                      padding: EdgeInsets.only(left: (width * 0.1)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Flexible(
+                            child: RichText(
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              text: TextSpan(
+                                text: list[i],
                                 style: const TextStyle(
-                                    fontSize: 14, fontWeight: FontWeight.w600),
+                                    color: Colors.black,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w600),
                               ),
-                            ],
+                            ),
                           ),
-                        ),
+                        ],
                       ),
                     ),
                   ),
                 ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              if (page > 0)
-                TextButton(
-                  child: const Text('<<'),
-                  onPressed: () {
-                    page -= 1;
-                    lw = pageList(context, height, width);
-                    setState(() {});
-                  },
-                )
-              else
-                TextButton(
-                  child: const Text(''),
-                  onPressed: () {},
-                ),
-              const SizedBox(width: 20),
-              Text((page + 1).toString()),
-              const SizedBox(width: 20),
-              if (page < temp)
-                TextButton(
-                  child: const Text('>>'),
-                  onPressed: () {
-                    page += 1;
-                    lw = pageList(context, height, width);
-                    setState(() {});
-                  },
-                )
-              else
-                TextButton(
-                  child: const Text(''),
-                  onPressed: () {},
-                )
-            ],
-          ),
+              ),
+            ),
         ],
       ),
     ];
   }
 
   void search() {
+    list = [];
     // all에 포함되어있는 것만 찾아내서 list에 추가
     widget.keyword = widget.keyword.toLowerCase();
     for (String str in all) {
@@ -142,6 +221,7 @@ class _HealthInfoListScreenState extends State<HealthInfoListScreen> {
       }
     }
     list = list.toSet().toList();
+    print(list);
   }
 
   void choice() {
