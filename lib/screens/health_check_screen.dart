0f3e8541b8b742f0ff73bc04d3a17e2554b48ce4
JunYong11 <92434317+JunYong11@users.noreply.github.com>
2023-11-04 16:04:52 +0900

    d
    d

--- lib/screens/health_check_screen.dart
+++ lib/screens/health_check_screen.dart
@@ -54,19 +54,19 @@ class _healthCheckScreenState extends State<healthCheckScreen> {
     "눈",
     "골반"
   ];
-  static List<bool> responses = [];
-  int check = 0;
 
+  int check = 0;
   final _firestore = FirebaseFirestore.instance;
   final _auth = FirebaseAuth.instance;
   late bool isLogin = false;
   late User loggedInUser;
   late int point = 0;
-
+  static List<bool> responses = [];
+  String name = '용가리'; //임의 닉네임
+  String result = "";
   late final userPoint = <String, dynamic>{
     "point": point,
   };
-
   @override
   void initState() {
     // TODO: implement initState
@@ -152,8 +152,17 @@ class _healthCheckScreenState extends State<healthCheckScreen> {
                       Icons.arrow_forward,
                       size: 40,
                     ),
-                    onPressed: () {
-                      validateAndSubmit();
+                    onPressed: () async {
+                      isLogin
+                          ? validateAndSubmit()
+                          : showDialog(
+                              context: context,
+                              builder: (context) {
+                                return const AlertDialog(
+                                  content: Text("로그인이 필요한 서비스 입니다."),
+                                );
+                              },
+                            );
                     }),
               ],
             ),
@@ -207,8 +216,6 @@ class _healthCheckScreenState extends State<healthCheckScreen> {
   }
 
   void validateAndSubmit() {
-    String result = "";
-    String name = '용가리';
     for (int i = 0; i < que.length; i++) {
       if (responses[i] == true) {
         result += "${que[i]},";
@@ -218,24 +225,19 @@ class _healthCheckScreenState extends State<healthCheckScreen> {
       HealthCheck().sendDataToJSP(name, result);
       //마일리지 올리기
       userPoint["point"] += 500;
-      print('check\n');
-      print(userPoint["point"]);
-      isLogin
-          ? _firestore
-              .collection("mileages")
-              .doc(loggedInUser.email)
-              .set(
-                userPoint,
-                SetOptions(merge: true),
-              )
-              .onError(
-                (e, _) => print("Error:$e"),
-              )
-          : print("not login!");
-
-      Navigator.of(context).push(
-        MaterialPageRoute(
-            builder: (context) => const MyInfoScreen()), // MyInfoScreen로 이동
+      _firestore
+          .collection("mileages")
+          .doc(loggedInUser.email)
+          .set(
+            userPoint,
+            SetOptions(merge: true),
+          )
+          .onError(
+            (e, _) => print("Error:$e"),
+          );
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => MyInfoScreen()),
       );
     } else {
       showDialog(
